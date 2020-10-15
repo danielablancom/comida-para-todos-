c7f0fa84839f6e62a6d5c2fd67dea179ecef9d3a
danielablancom <blancodanielac@gmail.com>
2020-10-15 00:51:10 -0300

    titles detail

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -5,22 +5,24 @@ import Imagen3 from "../Detail/IconDetail/Imagen3.png";
 import Imagen4 from "../Detail/IconDetail/Imagen4.png";
 import Imagen5 from "../Detail/IconDetail/Imagen5.png";
 import Imagen6 from "../Detail/IconDetail/Imagen6.png";
-import '../../assets/styles/Detail.scss'
-
+import "../../assets/styles/Detail.scss";
 
 function Detail() {
   return (
     <Fragment>
-
       <div className="containerDetails">
-      <div className="containerText">
-          <p>"Queremos entregar <strong>alimentación de calidad y nutritiva</strong>; conectando, colaborando y generando asociatividad entre los actores involucrados."</p>
+        <div className="containerText">
+          <p>
+            "Queremos entregar{" "}
+            <strong>alimentación de calidad y nutritiva</strong>; conectando,
+            colaborando y generando asociatividad entre los actores
+            involucrados."
+          </p>
         </div>
         <div className="ContainerText">
           <p>"Desde el 22 de mayo de 2020"</p>
         </div>
         <div className="containerBoxDetails">
-
           <div className="boxContainerDetail">
             <div className="container1">
               <img src={Imagen1} className="imgDetail" alt={"Imagen"} />
@@ -59,7 +61,6 @@ function Detail() {
               <h4>19</h4>
               <p>Pequeños Productores</p>
             </ul>
-
           </div>
 
           <div className="boxContainerDetail">
@@ -70,7 +71,6 @@ function Detail() {
               <h4>96</h4>
               <p>Puestos de Empleo</p>
             </ul>
-
           </div>
 
           <div className="boxContainerDetail">
@@ -81,11 +81,10 @@ function Detail() {
               <h4>12</h4>
               <p>Comunas Beneficiadas</p>
             </ul>
-
           </div>
         </div>
       </div>
     </Fragment>
   );
 }
-export default Detail
+export default Detail;
